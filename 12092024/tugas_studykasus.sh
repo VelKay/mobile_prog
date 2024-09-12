c6a336a7a48389f245fd8f13e05fd94834cc163e
VelKay <vela.ap13@gmail.com>
2024-09-12 13:29:00 +0700

    Latihan Studi Kasus Pertama

---
+++ 12092024/tugas_studykasus.sh
@@ -0,0 +1,39 @@
+function hitungharga() {
+    echo "Berapa jumlah developer?: "
+    read jumlahdev
+
+    echo "Berapa jumlah gaji per dev per bulan? (dalam Rupiah): "  
+    read gajidev
+
+    echo "Berapa bulan proyek ini dikerjakan?: "
+    read projectdur
+
+    echo "Berapa biaya server per bulan? (dalam Rupiah): "
+    read servercost
+    
+    echo "Berapa biaya license software? (dalam Rupiah): "
+    read licensecost
+
+    echo "Berapa biaya operasional lainnya? (dalam Rupiah): "
+    read operationcost
+
+    echo "Berapa anggaran awal proyek? (dalam Rupiah): "
+    read modal
+
+    echo "=========================="
+    echo "Jadi, total biaya gaji developernya adalah: Rp $totalgaji" 
+    echo "Jadi, total biaya servernya adalah: Rp $totalservercost"
+    echo "Jadi ,total biaya lisensi softwarenya adalah: Rp $licensecost"
+    echo "Jadi ,total biaya operasionalnya adalah: Rp $operationcost"
+    echo "=========================="
+    echo "Jadi, total biaya keseluruhan proyeknya adalah: Rp $totalcost"
+    echo "Jadi, sisa modalnya adalah: Rp $sisamodal" 
+
+    if [ $sisamodal -lt 0 ]; then
+    echo "Proyek ini melebihi anggaran sebesar Rp ${sisamodal#-}"
+    else
+    echo "Proyek ini dapat diselesaikan dengan anggaran yang ada."
+    fi
+}
+
+hitungharga
