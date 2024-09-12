9be5bb6d3ebb47977a034c8d1ce51f5ed653e743
VelKay <vela.ap13@gmail.com>
2024-09-12 10:27:33 +0700

    Testing

---
+++ 10092024/ScriptPertama.sh
@@ -0,0 +1,26 @@
+#echo "Siapa nama anda?"
+#command ini untuk berinteraksi dengan terminal
+#setelah "read" melupakan nama variale yang digunakan untuk menampung jawaban atau interkasi dari user atau pengguna terhadap terminal
+#read name
+#untuk memanggil variable yang kita gunakan untuk menampung jawaban atau interaksi gunakan $
+#echo "Nama saya adalah $name"
+
+#LATIHAN 1
+#echo "Masukan Panjang sebuah Persegi Panjang"
+#read p
+#echo "Masukan Lebar sebuah Persegi Panjang"
+#read l
+
+#k=$((2 * (p + l)))
+
+#echo "Keliling persegi panjang dengan panjang $p dan lebar $l adalah = $k"
+
+#LATIHAN 2
+#echo "Masukan jari-jari Kerucut"
+#read r
+#echo "Masukan tinggi Kerucut"
+#read t
+
+#v=$(((22/7) * r * r * t / 3))
+
+#echo "Volume Kerucut dengan jari-jari $r dan Tinggi $t adalah = $v"
